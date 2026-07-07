238f12826185e1334e1175e6314a2f191091b1f9
pkar <pkar@ieee.org>
2026-07-07 11:16:06 +0200

    test: proba vbUI nieudana

---
+++ vbUI/Info.xaml.vb
@@ -0,0 +1,24 @@
+﻿Imports pkar.UI.Extensions
+
+Public NotInheritable Class Info
+    Inherits Page
+
+    Private Sub bRateIt_Click(sender As Object, e As RoutedEventArgs)
+        App.OpenRateIt()
+    End Sub
+
+    Private Sub bAboutOk_Click(sender As Object, e As RoutedEventArgs)
+        Me.GoBack
+    End Sub
+
+    Private Sub OnLoaded(sender As Object, e As RoutedEventArgs) Handles uiInfoGrid.Loaded
+        Dim sFilename As String
+        sFilename = "Assets\Guide-" & Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName & ".htm"
+        If Not File.Exists(sFilename) Then sFilename = "Assets\Guide-En.htm"
+
+        ' błąd, albo np. jesteśmy pod Android :)
+        If Not File.Exists(sFilename) Then Return
+
+        uiWebInfo.NavigateToString(File.ReadAllText(sFilename))
+    End Sub
+End Class
---
+++ vbUI/MainPage.xaml.vb
@@ -0,0 +1,531 @@
+﻿
+Imports System.Net.Http
+Imports CommunityToolkit.WinUI
+Imports pkar.UI.Configs
+Imports pkar.UI.Extensions
+Imports pkar.UI.Triggers
+Imports vb14 = VBlib.pkarlibmodule14
+
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private inVb As New VBlib.MainPage(AddressOf UsunToast)
+
+    Public Sub ShowPostsList()
+        vb14.DumpCurrMethod()
+        ' wczytuje z sAllFeeds, przerabia to na krotka liste do lewego WebView
+
+        If VBlib.App.glItems Is Nothing Then Return
+
+        uiCount.Text = VBlib.App.glItems.Count & " items"
+        SetBadgeNo(VBlib.App.glItems.Count)
+
+        VBlib.App.bChangedXML = False
+
+        Select Case vb14.GetSettingsInt("uiSortOrder")
+            Case 1  ' by name
+                uiListItems.ItemsSource = From c In VBlib.App.glItems Order By c.sTitle
+            Case 2  ' by thread
+                uiListItems.ItemsSource = From c In VBlib.App.glItems Order By c.sFeedName, c.sTitle
+            Case Else ' 0, i nieznane: jak dotychczas
+                uiListItems.ItemsSource = From c In VBlib.App.glItems
+
+        End Select
+
+    End Sub
+
+    Private Async Sub uiLista_Click(sender As Object, e As TappedRoutedEventArgs)
+        Dim oItem As VBlib.JedenItem = TryCast(sender, Grid)?.DataContext
+        If oItem Is Nothing Then Return
+        Await ShowTorrentData(oItem)
+    End Sub
+
+
+    Public Async Function ShowTorrentData(oItem As VBlib.JedenItem) As Task
+        Dim sTmp As String
+        sTmp = "<html><body><h1>" & oItem.sTitle & "</h1>"
+        If oItem.sDate <> "" Then sTmp = sTmp & $"<p><small>Posted: {oItem.sDate}</small></p>"
+        sTmp = sTmp & oItem.sItemHtmlData
+
+        If oItem.sLinkToDescr.Contains("zdmk.krakow.pl") Then
+            Dim webclnt As Windows.Web.Http.HttpClient = KlientWeb()            ' System..Client zwraca httpcode 418
+            Try
+                sTmp &= Await webclnt.GetStringAsync(New System.Uri(oItem.sLinkToDescr))
+            Catch ex As Exception
+                sTmp &= ex.Message
+            End Try
+        End If
+
+        If oItem.sLinkToDescr.Contains("ekai.pl") Then
+
+            Dim webclnt As Windows.Web.Http.HttpClient = KlientWeb()            ' System..Client zwraca httpcode 418
+            Dim temppage As String
+
+            Try
+                temppage = Await webclnt.GetStringAsync(New System.Uri(oItem.sLinkToDescr))
+            Catch ex As Exception
+                temppage = ex.Message
+            End Try
+            ' Await VBlib.HttpPageAsync(oItem.sLinkToDescr)
+            Dim iInd = temppage.IndexOf("<article>")
+            If iInd > 1 Then
+                temppage = temppage.Substring(iInd)
+                iInd = temppage.IndexOf("</article>")
+                temppage = temppage.Substring(0, iInd + "</article>".Length)
+
+                sTmp &= temppage
+            End If
+        End If
+
+        If oItem.sLinkToDescr.Contains("lovekrakow.pl") Then
+
+            Dim webclnt As Windows.Web.Http.HttpClient = KlientWeb()
+
+            ' System..Client zwraca httpcode 418
+            Dim temppage As String = Await webclnt.GetStringAsync(New Uri(oItem.sLinkToDescr))
+            ' Await VBlib.HttpPageAsync(oItem.sLinkToDescr)
+            Dim iInd = temppage.IndexOf("<article id=""article")
+            If iInd > 1 Then
+                temppage = temppage.Substring(iInd)
+                iInd = temppage.IndexOf("<div id=""right_column")
+                iInd = temppage.LastIndexOf("</article>", iInd)
+                temppage = temppage.Substring(0, iInd + "</article>".Length)
+
+                sTmp &= temppage
+            End If
+        End If
+
+        sTmp &= "</body></html>"
+        uiPost.NavigateToString(sTmp)
+
+        If oItem.sLinkToDescr IsNot Nothing AndAlso oItem.sLinkToDescr.Contains("http") Then
+            uiBLink.NavigateUri = New Uri(oItem.sLinkToDescr)
+            sTmp = oItem.sLinkToDescr
+            ToolTipService.SetToolTip(uiBLink, sTmp)
+
+            'teraz różne wersje skracania pokazywanego linku
+            ' (2022.02.04: tutaj dopiero zmiana, w gItems jest pełnej długości, nie ma wcześniej skracania)
+
+            Dim iInd As Integer
+
+            ' próba 1: ucinamy po ?xxxx (m.in. obrazki instagram w ten sposób są traktowane)
+            If sTmp.Length > 30 Then
+                iInd = sTmp.IndexOf("?")
+                If iInd > 1 Then sTmp = sTmp.Substring(0, iInd)
+            End If
+
+            ' próba 2: ucięcie ścieżki w ramach serwera
+            If sTmp.Length > 30 Then
+                ' gdy link jest za długi, kasujemy ścieżkę w ramach serwera
+                Dim iInd1 As Integer
+                iInd = sTmp.IndexOf("/", 10)
+                iInd1 = sTmp.LastIndexOf("/")
+                sTmp = sTmp.Substring(0, iInd + 1) & "..." & sTmp.Substring(iInd1)
+            End If
+
+            ' próba 3: gdy sama filename jest długa
+            If sTmp.Length > 30 Then
+                iInd = sTmp.LastIndexOf("/")
+                If sTmp.Length - iInd > 20 Then sTmp = sTmp.Substring(0, iInd + 15) & "..."
+            End If
+            uiBLink.Content = sTmp
+        Else
+            uiBLink.NavigateUri = New Uri("http://127.0.0.1/")
+            uiBLink.Content = "<error>"
+            ToolTipService.SetToolTip(uiBLink, "")
+        End If
+
+        vb14.SetSettingsString("sLastId", oItem.sGuid)
+
+    End Function
+
+    Private Shared Function KlientWeb() As Windows.Web.Http.HttpClient
+        Dim webclnt As New Windows.Web.Http.HttpClient
+        webclnt.DefaultRequestHeaders.Accept.Clear()
+        webclnt.DefaultRequestHeaders.Accept.TryParseAdd("text/html")
+        webclnt.DefaultRequestHeaders.Accept.TryParseAdd("application/xhtml+xml")
+        webclnt.DefaultRequestHeaders.AcceptLanguage.Clear()
+        webclnt.DefaultRequestHeaders.AcceptLanguage.TryParseAdd("pl")
+        webclnt.DefaultRequestHeaders.UserAgent.Clear()
+        webclnt.DefaultRequestHeaders.UserAgent.TryParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0")
+        Return webclnt
+    End Function
+
+    Public Sub ShowTorrentData(sGuid As String)
+        'Dim sTmp As String
+
+        If VBlib.App.glItems Is Nothing Then
+            Me.MsgBox("IMPOSSIBLE, ShowTorrentData and oAllItems null")
+            Exit Sub
+        End If
+
+        If sGuid = "" Then Return   ' gdy byl pusty GUID (zbiorcze Toasty)
+
+        'DialogBox("ShowTorrentData(" & sGuid)
+
+        For Each oItem In VBlib.App.glItems
+            If oItem.sGuid = sGuid Then
+                ShowTorrentData(oItem)
+                Exit Sub
+            End If
+        Next
+
+        Me.MsgBox("IMPOSSIBLE? Cannot find such item")
+        Exit Sub
+
+    End Sub
+
+
+    ' obsluga zdarzen formatki
+    Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        Me.InitDialogs
+        Me.ProgRingInit(True, False)
+
+        AddHandler TryCast(Application.Current, App).UnhandledException, AddressOf GlobalError
+        CrashMessageInit()
+
+        'CrashMessageInit()
+        Await CrashMessageShowAsync()   ' jesli cos bylo, to pokaze tresc
+
+        AppResuming()   ' wczytaj listę RSSów
+        Await App.RegisterTriggers()
+
+        ' poniewaz nie dziala językowanie w tle, to trzeba teraz przepisac
+        'SetSettingsString("resDelete", GetLangString("resDelete"))
+        'SetSettingsString("resOpen", GetLangString("resOpen"))
+        'SetSettingsString("resBrowser", GetLangString("resBrowser"))
+        'SetSettingsString("resNewItemsInFeed", GetLangString("resNewItemsInFeed"))
+        'SetSettingsString("resNewItemsList", GetLangString("resNewItemsList"))
+
+
+#If DEBUG Then
+
+        ' bo tylko u mnie, i tylko gdy coś faktycznie jest, bo inaczej nie ma sensu
+        If VBlib.App.glItems IsNot Nothing AndAlso VBlib.App.glItems.Count > 1 AndAlso Not Await VBlib.DialogBoxYNAsync("Usunąć te które mają obrazek http 404?") Then Return
+
+        Dim oHttp As New HttpClient
+
+        Dim doUsun As New List(Of VBlib.JedenItem)
+
+        Me.ProgRingShow(True)
+        Dim iCnt As Integer = 0
+        For Each oItem As VBlib.JedenItem In VBlib.App.glItems
+            iCnt += 1
+            Me.ProgRingSetText(doUsun.Count & "/" & iCnt)
+            If Not oItem.sPicLink.StartsWith("https://devil-torrents.pl") Then Continue For
+
+            Dim oResp As HttpResponseMessage = Await oHttp.GetAsync(oItem.sPicLink)
+            If oResp.StatusCode = Net.HttpStatusCode.NotFound Then
+                doUsun.Add(oItem)
+            End If
+
+            Await Task.Delay(50)
+        Next
+
+        For Each oItem As VBlib.JedenItem In doUsun
+            VBlib.App.glItems.Remove(oItem)
+        Next
+        VBlib.App.bChangedXML = True
+
+        Me.ProgRingShow(False)
+
+        VBlib.MsgBox("Usunąłem " & doUsun.Count & " pozycji z obrazkiem 404.")
+        ShowPostsList()
+#End If
+
+    End Sub
+    Private Sub Form_Resized(sender As Object, e As SizeChangedEventArgs)
+        'uiLista.Width = uiNaList.ActualWidth - 20
+        'uiPost.Width = uiNaPost.ActualWidth - 20
+        'uiLista.Height = uiNaViews.ActualHeight - 20
+        'uiPost.Height = uiNaViews.ActualHeight - 20
+
+        If uiNaViews.ActualWidth < 200 Then
+            tbLastRead.Width = 0
+        ElseIf uiNaViews.ActualWidth < 600 Then
+            tbLastRead.Width = 100
+        Else
+            tbLastRead.Width = uiNaViews.ActualWidth / 2    ' nie wiecej niż pół szerokości
+        End If
+        'uiImage.Source = New ImageSource()
+        '    Image img = sender as Image; 
+        'BitmapImage BitmapImage = New BitmapImage();
+        'img.Width = BitmapImage.DecodePixelWidth = 80; //natural px width Of image source
+        '// don't need to set Height, system maintains aspect ratio, and calculates the other
+        '// dimension, so long as one dimension measurement Is provided
+        'BitmapImage.UriSource = New Uri(img.BaseUri, "Images/myimage.png");
+    End Sub
+
+
+    Private Sub Page_GotFocus(sender As Object, e As RoutedEventArgs)
+        vb14.DumpCurrMethod()
+        uiClockRead.GetSettingsBool() ' vb14.GetSettingsBool("autoRead")
+
+        ' 20171101: jeśli w środku jest moje, to nie rób reload
+        'Dim sHtml As String = ""
+
+        'Try
+        '    sHtml = Await uiLista.InvokeScriptAsync("eval", New String() {"document.documentElement.outerHTML;"})
+        'Catch ex As Exception
+        '    ' jesli strona jest pusta, jest Exception
+        'End Try
+
+        'If sHtml.IndexOf("<!-- FilteredRSS -->") < 1 Or GetSettingsBool("ChangedXML") Then ShowPostsList()
+        If VBlib.App.bChangedXML Then ShowPostsList()
+
+        tbLastRead.Text = vb14.GetSettingsString("lastRead")
+
+    End Sub
+
+#Region "commandbar"
+
+    Public Async Sub bReadFeed_Click(sender As Object, e As RoutedEventArgs)
+        vb14.DumpCurrMethod()
+
+        Dim sTmp As String = Await App.ReadFeeds(tbLastRead)
+
+        Try
+            tbLastRead.Text = sTmp
+            ShowPostsList()
+        Catch ex As Exception
+        End Try
+
+    End Sub
+    Private Sub uiClockRead_Click(sender As Object, e As RoutedEventArgs) Handles uiClockRead.Click
+        uiClockRead.SetSettingsBool()
+        If uiClockRead.IsChecked AndAlso Not IsTriggersRegistered("FilteredRSStimer") Then App.RegisterTriggers()
+    End Sub
+
+    Private Sub bSetup_Click(sender As Object, e As RoutedEventArgs)
+        'If uiNaViews.ActualWidth < 700 Then
+        '    Me.Frame.Navigate(GetType(SmallSetup))
+        'Else
+        Me.Navigate(GetType(Setup))
+        'End If
+    End Sub
+
+    Private Sub bDelOnePost_Click(sender As Object, e As RoutedEventArgs)
+        vb14.DumpCurrMethod()
+
+        Dim sGuid As String = vb14.GetSettingsString("sLastId")
+        If sGuid = "" Then Exit Sub
+
+        ' 20171101: ponieważ czasem seria kasowania powoduje crash - może dlatego że nie skończy poprzedniego kasowania jak zaczyna następne?
+        uiDelPost.IsEnabled = False
+
+        Dim sNextGuid As String = App.DelOnePost(sGuid)
+
+        Try
+            ShowPostsList()
+        Catch ex As Exception
+        End Try
+
+        If sNextGuid <> "" Then ShowTorrentData(sNextGuid)
+
+        uiDelPost.IsEnabled = True
+
+    End Sub
+
+    Private Sub bDelAllPosts_Click(sender As Object, e As RoutedEventArgs)
+        vb14.DumpCurrMethod()
+
+        vb14.SetSettingsString("sLastId", "")
+
+        Windows.UI.Notifications.ToastNotificationManager.History.Clear()
+
+        VBlib.App.glItems.Clear()
+        App.SaveIndex(True)
+
+        ShowPostsList()
+    End Sub
+
+    Private Sub bInfo_Click(sender As Object, e As RoutedEventArgs)
+        Me.Navigate(GetType(Info))
+    End Sub
+
+#End Region
+
+    ' obsluga Suspend/Resume (save data)
+    ' https://docs.microsoft.com/en-us/windows/uwp/launch-resume/suspend-an-app
+    ' oraz pliki
+    ' https://docs.microsoft.com/en-us/windows/uwp/app-settings/store-and-retrieve-app-data
+    Public Sub New()
+        InitializeComponent()
+        AddHandler Application.Current.Suspending, AddressOf AppSuspending
+        AddHandler Application.Current.Resuming, AddressOf AppResuming
+    End Sub
+
+    Public Sub AppSuspending()
+        vb14.DumpCurrMethod()
+        App.SaveIndex(True)
+    End Sub
+
+    Public Sub AppResuming()
+        App.LoadIndex()
+        App.KillFileLoad()
+        ShowPostsList()
+    End Sub
+
+
+    Private Sub uiPost_NavigationStarted(sender As WebView, args As WebViewNavigationStartingEventArgs) Handles uiPost.NavigationStarting
+        If args.Uri Is Nothing Then Exit Sub
+
+        args.Cancel = True
+#Disable Warning BC42358 ' Because this call is not awaited, execution of the current method continues before the call is completed
+        Windows.System.Launcher.LaunchUriAsync(args.Uri)
+#Enable Warning BC42358
+
+    End Sub
+
+    Private Sub uiBLink_RightTapped(sender As Object, e As RightTappedRoutedEventArgs) Handles uiBLink.RightTapped
+        uiBLink.NavigateUri.OpenBrowser(True)
+    End Sub
+
+    Public Function UsunToast(sGuid As String) As Boolean
+
+        Dim sTag As String = VBlib.App.ConvertGuidToTag(sGuid)
+        If sTag = "" Then Return False
+        Try
+            Windows.UI.Notifications.ToastNotificationManager.History.Remove(sTag)
+        Catch ex As Exception
+            ' chyba jeśli nie ma takowego - więc już usunięty jest
+        End Try
+        Return True
+    End Function
+
+#Region "ItemOnList-contextMenu"
+    Private Async Sub DeleteFromContextMenu(oMFI As MenuFlyoutItem, iMode As Integer)
+        vb14.DumpCurrMethod()
+        Dim oItem As VBlib.JedenItem = TryCast(oMFI?.DataContext, VBlib.JedenItem)
+        If oItem Is Nothing Then Return
+
+        Await inVb.DeleteFromContextMenu(oItem, iMode)
+
+        uiListItems.Focus(FocusState.Programmatic)
+
+        Try
+            ShowPostsList()
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Private Sub uiDelThis_Click(sender As Object, e As RoutedEventArgs)
+        DeleteFromContextMenu(sender, 1)
+    End Sub
+
+    Private Sub uiDelSubject_Click(sender As Object, e As RoutedEventArgs)
+        DeleteFromContextMenu(sender, 2)
+    End Sub
+
+    Private Sub uiDelFeed_Click(sender As Object, e As RoutedEventArgs)
+        DeleteFromContextMenu(sender, 3)
+    End Sub
+
+    Private Sub uiKillFile_Click(sender As Object, e As RoutedEventArgs)
+        DeleteFromContextMenu(sender, 4)
+    End Sub
+
+    Private Sub uiDelDoTego_Click(sender As Object, e As RoutedEventArgs)
+        DeleteFromContextMenu(sender, 5)
+    End Sub
+
+    Private Sub Page_Unloaded(sender As Object, e As RoutedEventArgs)
+        App.SaveIndex(False)
+    End Sub
+
+    Private Sub uiLista_KeyUp(sender As Object, e As KeyRoutedEventArgs)
+        'If e.Key <> Windows.System.VirtualKey.Space Then Exit Sub
+
+        'Dim fokus As ListViewItem = FocusManager.GetFocusedElement
+        'Dim fokCont = fokus.Content
+        'e.Handled = True
+        If e.Key = Windows.System.VirtualKey.Application Then
+
+            Dim lv = CType(sender, ListView)
+            Dim item = lv.SelectedItem
+            If item Is Nothing Then Return
+
+            ' Pobierz kontener ListViewItem
+            Dim container = CType(lv.ContainerFromItem(item), ListViewItem)
+            If container Is Nothing Then Return
+
+            ' Znajdź Grid z DataTemplate
+            Dim rootGrid = FindChild(Of Grid)(container, "Grid")
+            If rootGrid Is Nothing Then Return
+
+            '' Pobierz ten sam Flyout, który jest w XAML
+            'Dim flyout = FlyoutBase.GetAttachedFlyout(rootGrid)
+            'If flyout Is Nothing Then Return
+
+            '' Pokaż w tym samym miejscu co normalnie
+            'flyout.ShowAt(rootGrid)
+            rootGrid.ContextFlyout.ShowAt(rootGrid)
+
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Function FindChild(Of T As DependencyObject)(parent As DependencyObject, name As String) As T
+        Dim count = VisualTreeHelper.GetChildrenCount(parent)
+        For i = 0 To count - 1
+            Dim child = VisualTreeHelper.GetChild(parent, i)
+
+            If TypeOf child Is T Then
+                Return CType(child, T)
+            End If
+
+            Dim result = FindChild(Of T)(child, name)
+            If result IsNot Nothing Then Return result
+        Next
+        Return Nothing
+    End Function
+
+
+
+    Private selectionTimer As DispatcherTimer
+    Private lastSelectedItem As VBlib.JedenItem
+
+    Private Sub uiListItems_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+        If selectionTimer Is Nothing Then
+            selectionTimer = New DispatcherTimer()
+            AddHandler selectionTimer.Tick, AddressOf OnSelectionTimerTick
+            selectionTimer.Interval = TimeSpan.FromSeconds(2)
+        End If
+
+        Dim lv = CType(sender, ListView)
+
+        ' Jeśli nic nie zaznaczono – zatrzymaj timer
+        If lv.SelectedItem Is Nothing Then
+            selectionTimer.Stop()
+            lastSelectedItem = Nothing
+            Return
+        End If
+
+        ' Zapamiętaj aktualnie zaznaczony element
+        lastSelectedItem = TryCast(lv.SelectedItem, VBlib.JedenItem)
+
+        ' Restart timera
+        selectionTimer.Stop()
+        selectionTimer.Start()
+
+    End Sub
+
+    Private Sub OnSelectionTimerTick(sender As Object, e As Object)
+        selectionTimer.Stop()
+
+        If lastSelectedItem Is Nothing Then Return
+        ' Wywołaj swoją metodę
+        ShowTorrentData(lastSelectedItem)
+    End Sub
+
+    Private Sub uiBLink_Click(sender As Object, e As RoutedEventArgs)
+        uiListItems.Focus(FocusState.Programmatic)
+    End Sub
+
+#End Region
+
+End Class
